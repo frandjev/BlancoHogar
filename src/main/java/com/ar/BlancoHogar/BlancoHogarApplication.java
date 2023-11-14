83b0ad152d8119fafdd4f232aecd2ff067cac797
Franco Maidana <frandjev@gmail.com>
2023-11-13 23:52:52 -0300

    Inicio del proyecto

---
+++ src/main/java/com/ar/BlancoHogar/BlancoHogarApplication.java
@@ -0,0 +1,13 @@
+package com.ar.BlancoHogar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlancoHogarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlancoHogarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ar/BlancoHogar/BlancoHogarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.BlancoHogar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlancoHogarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
